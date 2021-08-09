22eb192df92dbe74e6346de5d41c13b2586c9de2
Daniel Diga <danieldeega@gmail.com>
2021-08-09 20:17:20 +0300

    Users

--- twenty-twenty-child/functions.php
+++ twenty-twenty-child/functions.php
@@ -8,3 +8,14 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'twentytwenty-child-style', get_stylesheet_uri(), array( $parent ), filemtime( dirname( __FILE__ ) . '/style.css' ) );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+// Remove admin bar for wp-test 
+function remove_admin_bar()
+{
+    $user = wp_get_current_user();
+    $username = $user->user_login;
+    if ($username && $username == "wp-test") {
+        show_admin_bar(false);
+    }
+}
+add_action('after_setup_theme', 'remove_admin_bar');
